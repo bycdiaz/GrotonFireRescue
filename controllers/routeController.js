fb2e7c53dad593bd7c93a367f73bb36d946071e3
Briggs Elsperger <briggse@protonmail.com>
2018-02-12 21:51:39 -0600

    moves some to adminController

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,14 +1,3 @@
 exports.home = (req, res) => {
   res.render('homePage');
 }
-<<<<<<< HEAD
-=======
-
-express.adminLogin = (req, res) => {
-  res.render('admin/adminLogin');
-}
-
-express.adminPanel = (req, res) => {
-  res.render('admin/adminPanel')
-}
->>>>>>> 8a5c8d173248fda41552bf0e002695182ab58d73
