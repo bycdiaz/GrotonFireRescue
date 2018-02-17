333f32af515b440beb16b6bd343fce47927d0e88
Briggs Elsperger <briggse@protonmail.com>
2018-02-16 23:10:18 -0600

    refactor and adds reminder

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,14 +6,11 @@ const authController = require('../controllers/authController');
 
 router.get('/', adminController.loginForm);
 router.post('/', authController.login);
-router.get('/add', adminController.addAdminForm);
-
-router.post('/add', 
-  adminController.registerNewAdmin
-);
-
+router.get('/add', adminController.addAdminForm); // TODO: Set so only superAdmin can add users
+router.post('/add', adminController.registerNewAdmin); 
 router.get('/logout', authController.logout)
 
+// Panel Items
 router.get('/admin-panel', authController.isLoggedIn, adminController.adminPanel);
 router.get('/fire-fighters', authController.isLoggedIn, adminController.editFireFighters);
 
