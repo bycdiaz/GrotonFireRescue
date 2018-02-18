681568ece1c5c38db48a2bf9078291b3b40abd5e
I3uckwheat <briggse@protonmail.com>
2018-02-18 15:25:00 -0600

    adds missing routes for rescue squad

--- routes/infoRoutes.js
+++ routes/infoRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const infoController = require('../controllers/infoController');
 
 router.get('/', infoController.index);
+
 router.get('/groton', infoController.aboutGroton);
 router.get('/groton/edit', infoController.editAboutGroton);
 router.post('/groton/edit', infoController.updateAboutGroton);
@@ -14,6 +15,8 @@ router.get('/fire-tax-district/edit', infoController.editFireTaxDistrict);
 router.post('/fire-tax-district/edit', infoController.updateFireTaxDistrict);
 
 router.get('/rescue-squad', infoController.aboutRescueSquad);
+router.get('/rescue-squad/edit', infoController.editAboutRescueSquad);
+router.post('/rescue-squad/edit', infoController.updateAboutRescueSquad);
 
 
 module.exports = router;
