94d7dd5c0d8947ae66025539278714a4454241dd
I3uckwheat <briggse@protonmail.com>
2018-02-18 12:20:15 -0600

    adds virtual field for position

--- models/Firefighter.js
+++ models/Firefighter.js
@@ -12,11 +12,23 @@ const firefighterSchema = new mongoose.Schema({
       required: true
     },
   },
-  position: {
-    type: String,
-    default: "Firefighter"
-  },
-  yearsOfService: Number
+  rank: {
+    type: Number,
+    default: 9
+  }
 });
+
+firefighterSchema.virtual('position').get(function(){
+  const positions = {0:"Chief", 
+                    1:"First Assisstant Chief", 
+                    2:"Second Assistant Chief",
+                    3:"First Captain", 
+                    4:"Second Captain", 
+                    5:"Third Captain",
+                    6:"President",
+                    7:"Vice President",
+                    8:"Secretary/Treasurer",
+                    9:"Firefighter"}
+})
 
 module.exports = mongoose.model("Firefighter", firefighterSchema);
