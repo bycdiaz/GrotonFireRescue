1598dc8a82dcfd57f2c51137adc3323dde9d12d6
I3uckwheat <briggse@protonmail.com>
2018-02-24 00:15:14 -0600

    starts refactoring

--- controllers/infoController.js
+++ controllers/infoController.js
@@ -6,93 +6,30 @@ exports.index = (req, res) => {
   res.render('info/aboutIndex');
 };
 
+const pageIDMap = {"fire-tax-district":0, "groton":1, "rescue-squad":2};
+
 
 // About Groton
-exports.aboutGroton = (req, res, next) => {
-  Page.findOne({pageID: 1})
-    .then((page)=> {
-      if(!page) page = {};
-      res.render('info/aboutGroton', {page});
-    })
-    .catch((err) => {
-      next(err);
-    });
+exports.aboutPage = (req, res, next) => {
+  
 };
 
-exports.editAboutGroton = (req, res, next) => {
+exports.editAboutPage = (req, res, next) => {
   Page.findOne({pageID: 1})
     .then((page) => {
       if(!page){page = {}};      
-      res.render('info/editAbout', {page})
+      res.render('info/editPage', {page})
     })
     .catch((err) => {
       next(err);
     })
 };
 
-exports.updateAboutGroton = (req, res, next) => {
+exports.updateAboutPage = (req, res, next) => {
   Page.findOneAndUpdate({pageID: 1}, req.body, {upsert: true, runValidators: true, setDefultsOnInsert: true, new: true})
     .then((page) => {
       req.flash("success", "SUCCESSFULLY UPDATED")
       res.redirect('/about/groton');
-    })
-    .catch((err) => next(err));
-};
-
-
-// About fire tax district
-exports.aboutFireTaxDistrict = (req, res, next) => {
-  Page.findOne({pageID: 0})
-    .then((page) => {
-      if(!page) page = {};
-      res.render('info/aboutFireTaxDistrict', {page});
-    })
-    .catch((err) => next(err));
-};
-
-exports.editFireTaxDistrict = (req, res, next) => {
-  Page.findOne({pageID: 0})
-    .then((page) => {
-      if(!page) page = {};      
-      res.render('info/editAbout');
-    })
-    .catch((err) => next(err));
-}
-
-exports.updateFireTaxDistrict = (req, res, next) => {
-  Page.findOneAndUpdate({pageID: 0}, req.body, {upsert: true, runValidators: true, setDefultsOnInsert: true, new: true})
-  .then((page) => {
-    req.flash("success", "SUCCESSFULLY UPDATED")
-    res.redirect('/about/fire-tax-district');
-  })
-  .catch((err) => next(err));
-}
-
-
-//About rescue squad
-exports.aboutRescueSquad = (req, res, next) => {
-  Page.findOne({pageID: 2})
-    .then((page) => {
-      if(!page) page = {};      
-      res.render('info/aboutRescueSquad', {page});
-    })
-    .catch((err) => next(err));
-};
-
-exports.editAboutRescueSquad = (req, res, next) => {
-  Page.findOne({pageID: 2})
-    .then((page) => {
-      if(!page) page = {};  
-      res.render('info/editAbout', {page});
-    })
-    .catch((err) => next(err));
-};
-
-exports.updateAboutRescueSquad = (req, res, next) => {
-  Page.findOneAndUpdate({pageID: 2}, req.body, {upsert: true, runValidators: true, setDefultsOnInsert: true, new: true})
-    .then((page) => {
-      req.flash("success", "SUCCESSFULLY UPDATED")
-      res.redirect('/about/rescue-squad');
     })
     .catch((err) => next(err));
 };
--- routes/infoRoutes.js
+++ routes/infoRoutes.js
@@ -5,18 +5,8 @@ const infoController = require('../controllers/infoController');
 
 router.get('/', infoController.index);
 
-router.get('/groton', infoController.aboutGroton);
-router.get('/groton/edit', infoController.editAboutGroton);
-router.post('/groton/edit', infoController.updateAboutGroton);
-
-
-router.get('/fire-tax-district', infoController.aboutFireTaxDistrict);
-router.get('/fire-tax-district/edit', infoController.editFireTaxDistrict);
-router.post('/fire-tax-district/edit', infoController.updateFireTaxDistrict);
-
-router.get('/rescue-squad', infoController.aboutRescueSquad);
-router.get('/rescue-squad/edit', infoController.editAboutRescueSquad);
-router.post('/rescue-squad/edit', infoController.updateAboutRescueSquad);
-
+router.get('/:page', infoController.aboutPage);
+router.get('/:page/edit', infoController.editAboutPage);
+router.post('/:page/edit', infoController.updateAboutPage);
 
 module.exports = router;
