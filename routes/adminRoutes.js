3487a7b5383df62dd019c88575fc5fe5ea923925
I3uckwheat <briggse@protonmail.com>
2018-03-03 15:23:23 -0600

    conforms to standards

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,8 +7,8 @@ const authController = require('../controllers/authController');
 router.get('/', adminController.loginForm);
 router.post('/', authController.login);
 router.get('/add', adminController.addAdminForm); // TODO: Set so only superAdmin can add admins
-router.post('/add', adminController.registerNewAdmin); 
-router.get('/logout', authController.logout)
+router.post('/add', adminController.registerNewAdmin);
+router.get('/logout', authController.logout);
 router.get('/panel', adminController.adminPanel);
 
 module.exports = router;
