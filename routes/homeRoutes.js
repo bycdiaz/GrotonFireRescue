7f1391e816373e7174773be265335650f045c96e
I3uckwheat <briggse@protonmail.com>
2018-03-03 16:15:56 -0600

    adds routes for homeController

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -4,3 +4,9 @@ const router = express.Router();
 
 const homeController = require('../controllers/homeController.js');
 
+router.get('/', homeController.home);
+router.get('/edit', homeController.editHome);
+router.post('/', homeController.updateHome);
+
+module.exports = router;
+
