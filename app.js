5a4ef83f7bb328c3a1eed3200bc32245afd0d55a
I3uckwheat <briggse@protonmail.com>
2018-03-03 15:19:23 -0600

    sets up basic admin bar

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
   res.locals.h = helpers;
   res.locals.flashes = req.flash();
   res.locals.currentPath = req.path;
-  // res.locals.user = req.user || null;
+  res.locals.user = req.user || null;
   next();
 });
 
