875e18f21463027f1c791eade2b0ce7bc199c993
I3uckwheat <briggse@protonmail.com>
2018-03-07 23:40:51 -0600

    renames to galleryRoutes

--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const pictureController = require('../controllers/pictureController');
+const galleryController = require('../controllers/galleryController');
 
 // TODO - all below
 
-router.get('/', pictureController.showGallery); // Shows Categories with thumbnail
-router.get('/:category', pictureController.showGallery); // Shows images from the category
+router.get('/', galleryController.showGallery); // Shows Categories with thumbnail
+router.get('/:category', galleryController.showGallery); // Shows images from the category
 router.get('/:category/:imageName');
 
 router.get('/admin'); // Two panes, one for uploading, one for deleting
