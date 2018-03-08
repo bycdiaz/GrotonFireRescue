5223b09cae169925f7aab4f9fab3128ad9408d10
I3uckwheat <briggse@protonmail.com>
2018-03-07 23:20:03 -0600

    changes routes ideas to be easier to work with

--- routes/picturesRoutes.js
+++ routes/picturesRoutes.js
@@ -6,14 +6,14 @@ const pictureController = require('../controllers/pictureController');
 
 // TODO - all below
 
-router.get('/', pictureController.getCategories, pictureController.showPictures); // Shows Categories with thumbnail
-router.get('/:category', pictureController.getImages, pictureController.showPictures); // Shows images from the category
-router.get('/:category/:image', pictureController.getSingleImage);
+router.get('/', pictureController.showGallery); // Shows Categories with thumbnail
+router.get('/:category', pictureController.showGallery); // Shows images from the category
+router.get('/:category/:imageName');
 
-router.get('/admin', pictureController.edit); // Two panes, one for uploading, one for deleting
-router.post('/:category/:image', pictureController.addImage); // Finds directory and creates it if it doesn't exist, adds image to category
-router.post('/admin/delete/:category', pictureController.deleteCategory); // Deletes entire Category
-router.post('/admin/delete/:category/:imageName', pictureController.deleteImage);// Deletes one image
+router.get('/admin'); // Two panes, one for uploading, one for deleting
+router.post('/:category/:image'); // Finds directory and creates it if it doesn't exist, adds image to category
+router.post('/admin/delete/:category'); // Deletes entire Category
+router.post('/admin/delete/:category/:imageName');// Deletes one image
 
 
 module.exports = router;
