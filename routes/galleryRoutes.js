9ad7b18d2d77fa69bde04ecf74f8206a20b630f0
Briggs Elsperger <briggse@protonmail.com>
2018-03-14 20:11:42 -0500

    adds route

--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -8,11 +8,13 @@ const galleria = require('../handlers/gallery');
 const gallery = galleria();
 
 router.get('/', gallery.getIndex(), galleryController.index);
+
 router.get('/edit', galleryController.editGallery);
 router.get('/edit/categorylist', gallery.getIndex({ json: true }));
 router.get('/edit/:category', gallery.getImagesFromCategory({ json: true }));
 
-router.get('/:category', gallery.getImagesFromCategory, galleryController.category);
+router.get('/:category', gallery.getImagesFromCategory(), galleryController.category);
+router.post('/:cateogry', gallery.upload());
 
 
 module.exports = router;
