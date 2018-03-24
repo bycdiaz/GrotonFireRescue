cc1d6f272a8da67a46b09b07c8ecb22a3ca7cac5
Briggs Elsperger <briggse@protonmail.com>
2018-03-24 13:33:12 -0500

    adds reset routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,8 +7,9 @@ const authController = require('../controllers/authController');
 router.get('/', adminController.loginForm);
 router.post('/', authController.login);
 router.get('/add', adminController.addAdminForm); // TODO: Set so only superAdmin can add admins
-router.post('/add', adminController.registerNewAdmin);
+router.post('/add', authController.register);
 router.get('/logout', authController.logout);
 router.get('/panel', adminController.adminPanel);
-
+router.get('/password-reset', authController.resetPasswordPage);
+router.post('/password-reset', authController.resetPassword);
 module.exports = router;
