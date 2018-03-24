ee776ef48376e1a67c59f0a84c60056bb5ed85c0
Briggs Elsperger <briggse@protonmail.com>
2018-03-24 13:32:40 -0500

    adds default password and forced reset with token

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,16 +3,34 @@ const mongoose = require('mongoose');
 
 const Admin = mongoose.model('Admin');
 
-exports.login = passport.authenticate('local', {
-  failureRedirect: '/admin',
-  failureFlash: 'Invalid email address or password',
-  successRedirect: '/admin/panel',
-  successFlash: 'You are now logged in',
-});
+exports.login = (req, res, next) => {
+  passport.authenticate('local', (err, user) => {
+    if (err) { return next(err); }
+
+    if (!user) {
+      // req.flash('failure', 'You must be logged in to do that');
+      console.log('no user');
+      return res.redirect('/admin');
+    }
+
+    if (user.firstLogin) {
+      req.flash('failure', 'Please create your password using the code provided');
+      console.log(user);
+      return res.redirect('/admin/password-reset');
+    }
+
+    // TODO - allow user to reset own password
+
+    req.logIn(user, (err) => {
+      if (err) { return next(err); }
+      return res.redirect('/admin/panel');
+    });
+  })(req, res, next);
+};
 
 exports.logout = (req, res) => {
   req.logout();
-  req.flash('success', 'You are now logged out');
+  // req.flash('success', 'You are now logged out'); // TODO - style flashes
   res.redirect('/');
 };
 
@@ -29,3 +47,67 @@ exports.isSuperAdmin = (req, res, next) => {
   req.flash('You must be a superAdmin to do that');
   return res.redirect('/admin');
 };
+
+exports.register = (req, res, next) => {
+  const token = randomResetToken();
+  const admin = new Admin({
+    email: req.body.email,
+    name: req.body.name,
+    resetToken: { token },
+  });
+
+  Admin.register(admin, process.env.DEFAULT_ADMIN_PASSWORD, (err, user) => {
+    if (err) return next(err);
+
+    req.flash('success', `${user.name} has been registered, the reset token is: ${token}`);
+    return res.redirect('/admin/panel');
+  });
+};
+
+exports.resetPasswordPage = (req, res) => { // TODO - test
+  res.render('admin/passwordReset', { user: req.params.user, token: req.params.token });
+};
+
+exports.resetPassword = (req, res, next) => {
+  passport.authenticate('local', (err, user) => {
+    if (err) { return next(err); }
+
+    if (!user) {
+      req.flash('failure', 'Email or password is incorrect');
+      return res.redirect('/admin/password-reset');
+    }
+
+    if (!validateToken(user, req.body.resetToken)) {
+      req.flash('failure', 'Please change your password using the provided code and default password');
+      return res.redirect('/admin/password-reset');
+    }
+
+    if (req.body.newPassword !== req.body.confirmPassword) {
+      req.flash('failure', 'Passwords do not match');
+      return res.redirect('/admin/password-reset');
+    }
+
+    user.passReset = false;
+    user.firstLogin = false;
+    user.changePassword(req.body.password, req.body.newPassword, (err) => {
+      if (err) return next(err);
+      req.logIn(user, (err) => {
+        if (err) { return next(err); }
+        return res.redirect('/admin/panel');
+      });
+    });
+    user.save();
+  })(req, res, next);
+};
+
+
+function randomResetToken() {
+  return Math.floor(Math.random() * (9999 - (1111 + 1))) + 1111;
+}
+
+function validateToken(user, token) {
+  if (user.resetToken.expires > Date.now() && user.resetToken.token === token) {
+    return true;
+  }
+  return false;
+}
