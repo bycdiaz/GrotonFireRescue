4a39c324673cff8644ab14d1620e2d5be33d0f0a
Briggs Elsperger <briggse@protonmail.com>
2018-03-26 20:13:21 -0500

    protects admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,14 +6,19 @@ const authController = require('../controllers/authController');
 
 router.get('/', adminController.loginForm);
 router.post('/', authController.login);
-router.get('/add', adminController.addAdminForm); // TODO: Set so only superAdmin can add admins
-router.post('/add', authController.register);
-router.get('/logout', authController.logout);
-router.get('/panel', adminController.adminPanel);
 router.get('/password-reset', authController.resetPasswordPage);
 router.post('/password-reset', authController.resetPassword);
+router.get('/logout', authController.logout);
 
+router.use(authController.isLoggedIn);
+router.get('/panel', adminController.adminPanel);
+router.get('/reset-my-password', authController.selfResetPasswordForm);
+router.post('/reset-my-password', authController.selfResetPassword);
+
+router.use(authController.isLoggedIn, authController.isSuperAdmin);
+router.get('/add', adminController.addAdminForm); // TODO: Set so only superAdmin can add admins
+router.post('/add', authController.register);
 router.post('/:id/remove', authController.removeAdmin);
-router.post('/:id/resetPassword', authController.resetPassword);
+router.post('/:id/resetPassword', authController.forceResetPassword);
 // router.post('/:id/setSuperAdmin', authController.setSuperAdmin); // TODO - maybe
 module.exports = router;
