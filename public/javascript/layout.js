b86b1e7b270f8623c487dac13af3551432cc02a3
Briggs Elsperger <briggse@protonmail.com>
2018-04-08 15:05:15 -0500

    adds listener to screen size

--- public/javascript/layout.js
+++ public/javascript/layout.js
@@ -1,6 +1,33 @@
+let smallScreenModeActive = false;
+
 document.querySelectorAll('.flash').forEach((flash) => {
   flash.addEventListener('click', function handler(e) {
-    e.target.parentNode.innerHTML = "";
+    e.target.parentNode.innerHTML = '';
     this.removeEventListener('click', handler);
   });
 });
+
+if (window.innerWidth <= 635) activateSmallScreenMode();
+
+window.onresize = resizeHandler;
+
+function resizeHandler() {
+  if (!smallScreenModeActive && window.innerWidth <= 635) {
+    activateSmallScreenMode();
+  } else if (smallScreenModeActive && window.innerWidth > 635) {
+    activateStandardMode();
+  }
+}
+
+
+function activateSmallScreenMode() {
+  smallScreenModeActive = true;
+  document.querySelectorAll('nav a').forEach((a) => {
+    a.classList.add('js-hidden');
+  });
+
+}
+
+function activateStandardMode() {
+  smallScreenModeActive = false;
+}
