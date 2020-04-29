9a5d1e0a0fbaa01b510131b0e60d82f4a8d731a6
Carlos Diaz <cdiaz86@gmail.com>
2020-04-28 22:30:24 -0400

    Adds click event to year elements.

--- public/javascript/gunDrawing.js
+++ public/javascript/gunDrawing.js
@@ -1,6 +1,18 @@
-const dates = document.querySelectorAll('.date');
-console.log(dates);
+const years = document.querySelectorAll(".years")
 
-dates.forEach(element => {
-  element.parentElement.setAttribute("class", "card visible")
+years.forEach((year) => {
+  year.addEventListener('click', (event) => {
+    console.log(event.target);
+    
+    // const dates = document.querySelectorAll('.date');
+    // // console.log(dates);
+
+    // dates.forEach(element => {
+    //   console.log(element.innerText);
+      
+    //   if (element.innerText.includes("2020")) {
+    //     element.parentElement.setAttribute("class", "card visible");
+    //   }
+    // });
+  });
 });
