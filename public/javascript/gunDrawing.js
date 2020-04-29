823e3d4ebc2bb6cf98ff52b6e79e368c9e4b501a
Carlos Diaz <cdiaz86@gmail.com>
2020-04-29 19:23:44 -0400

    Focuses selected tab.

--- public/javascript/gunDrawing.js
+++ public/javascript/gunDrawing.js
@@ -1,18 +1,8 @@
-const years = document.querySelectorAll(".years")
+const years = document.querySelector(".years");
 
-years.forEach((year) => {
-  year.addEventListener('click', (event) => {
-    console.log(event.target);
-    
-    // const dates = document.querySelectorAll('.date');
-    // // console.log(dates);
-
-    // dates.forEach(element => {
-    //   console.log(element.innerText);
-      
-    //   if (element.innerText.includes("2020")) {
-    //     element.parentElement.setAttribute("class", "card visible");
-    //   }
-    // });
+years.addEventListener('click', (event) => {
+  years.childNodes.forEach(year => {
+    year.className = "year"
   });
-});
+  event.target.className = "year current" 
+})
