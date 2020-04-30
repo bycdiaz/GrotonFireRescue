6d2b1619a88b5182f305f06cbabcf4657300494d
Carlos Diaz <cdiaz86@gmail.com>
2020-04-29 20:03:13 -0400

    Displays winners on tab click.

--- public/javascript/gunDrawing.js
+++ public/javascript/gunDrawing.js
@@ -1,8 +1,28 @@
 const years = document.querySelector(".years");
+const cardDates = document.querySelectorAll(".date");
 
-years.addEventListener('click', (event) => {
-  years.childNodes.forEach(year => {
-    year.className = "year"
-  });
-  event.target.className = "year current" 
-})
+function focusYearTab() {
+  years.addEventListener('click', (event) => {
+    console.log(event.target.innerText);
+    
+    years.childNodes.forEach(year => {
+      console.log(year.innerHTML);
+      
+      year.className = "year";
+    });
+    event.target.className = "year current";
+
+    cardDates.forEach(date => {
+      const winnerYear = date.innerHTML.slice(-4);
+      const tabYear = event.target.innerText;
+      
+      if (winnerYear === tabYear) {
+        date.parentNode.className = "card"
+      } else {
+        date.parentNode.className = "card hidden"
+      }
+    })
+  })
+}
+
+focusYearTab();
