238fe3f29eef632f2d0d77370b17eacff7f1f79e
Carlos Diaz <cdiaz86@gmail.com>
2020-04-29 20:07:01 -0400

    Moves winner display logic into new function.

--- public/javascript/gunDrawing.js
+++ public/javascript/gunDrawing.js
@@ -1,27 +1,27 @@
 const years = document.querySelector(".years");
 const cardDates = document.querySelectorAll(".date");
 
+function winnerDisplay() {
+  cardDates.forEach(date => {
+    const winnerYear = date.innerHTML.slice(-4);
+    const tabYear = event.target.innerText;
+    
+    if (winnerYear === tabYear) {
+      date.parentNode.className = "card"
+    } else {
+      date.parentNode.className = "card hidden"
+    }
+  })
+}
+
 function focusYearTab() {
   years.addEventListener('click', (event) => {
-    console.log(event.target.innerText);
-    
     years.childNodes.forEach(year => {
-      console.log(year.innerHTML);
-      
       year.className = "year";
     });
     event.target.className = "year current";
 
-    cardDates.forEach(date => {
-      const winnerYear = date.innerHTML.slice(-4);
-      const tabYear = event.target.innerText;
-      
-      if (winnerYear === tabYear) {
-        date.parentNode.className = "card"
-      } else {
-        date.parentNode.className = "card hidden"
-      }
-    })
+    winnerDisplay();
   })
 }
 
